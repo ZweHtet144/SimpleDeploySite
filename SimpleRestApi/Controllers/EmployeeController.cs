89a2f260ac26a38bdabcb5a0801d14b664c076a9
zwewaiyanhtet <zwewaiyanhtet@YB-LT2011>
2022-12-26 15:31:12 +0630

    -

---
+++ SimpleRestApi/Controllers/EmployeeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SimpleRestApi.Models;
+
+namespace SimpleRestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : Controller
+    {
+        [HttpPost]
+        [Route("search")]
+        public async Task<string> GetEmployeeList([FromBody] RequestModel reqModel)
+        {
+            EmployeeModel employee = new EmployeeModel();
+            if (reqModel != null)
+            {
+                employee.Id = Guid.NewGuid().ToString();
+                employee.EmployeeID = "009807";
+                employee.FullName = "ZweWaiYanHtet";
+                employee.DisplayName = "ZweWaiYanHtet";
+                employee.StartDate = "";
+                employee.Division = "Technology";
+                employee.JobTitle = "Developer";
+                employee.Mobile = "09975638342";
+                employee.OfficeNumber = "YB2";
+                employee.Mail = "zwewaiyanhtet@yomabank.com";
+                employee.Department = "Architecture Office";
+                employee.Section = "AO";
+                employee.Manager = "AungMyoAye";
+                employee.City = "Yangon";
+                employee.BranchCode = "0057";
+                employee.Office = "Head Office";
+            }
+            return await Task.FromResult(JsonConvert.SerializeObject(employee));
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public string UpdateEmployee([FromBody] EmployeeModel reqModel)
+        {
+            ResponseModel response = new ResponseModel();
+            if (reqModel != null)
+            {
+                response.Code = "200";
+                response.Message = "Success";
+                goto Results;
+            }
+            response.Code = "999";
+            response.Message = "Error";
+        Results:
+            return JsonConvert.SerializeObject(response);
+        }
+    }
+}
---
+++ SimpleRestApi/Models/EmployeeModel.cs
@@ -0,0 +1,32 @@
+﻿namespace SimpleRestApi.Models
+{
+    public class RequestModel
+    {
+        public string EmployeeId { get; set; }
+    }
+    public class EmployeeModel
+    {
+        public string Id { get; set; }
+        public string EmployeeID { get; set; }
+        public string FullName { get; set; }
+        public string DisplayName { get; set; }
+        public string StartDate { get; set; }
+        public string Division { get; set; }
+        public string JobTitle { get; set; }
+        public string Mobile { get; set; }
+        public string OfficeNumber { get; set; }
+        public string Mail { get; set; }
+        public string Department { get; set; }
+        public string Section { get; set; }
+        public string Manager { get; set; }
+        public string City { get; set; }
+        public string BranchCode { get; set; }
+        public string Office { get; set; }
+    }
+
+    public class ResponseModel
+    {
+        public string Code { get; set; }
+        public string Message { get; set; }
+    }
+}
